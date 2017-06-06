3b31813a44c42a866d2f45a35161e2d91c576f2a
vcordero07 <vcordero07@gmail.com>
2017-06-05 22:09:42 -0400

    wip-weatherinfo

--- app.js
+++ app.js
@@ -8,38 +8,54 @@
 
 //http://realtime.mbta.com/developer/api/v2/stopsbyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=69&format=json
 
+//wunderground
+//http://api.wunderground.com/api/281d8cd199da64f2/conditions/q/42.370772,-71.076536.json
+
 
 let MBTARoutesEndPoint = 'http://realtime.mbta.com/developer/api/v2/routes';
 let MBTABusStopEndPoint = 'http://realtime.mbta.com/developer/api/v2/stopsbyroute'; //?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=69&format=json
-let WUEndPoint = '';
+let WUEndPoint = 'http://api.wunderground.com/api/281d8cd199da64f2/conditions/q/'; //q/42.370772,-71.076536.json
 
 // let MBTAApiKey = '1VI-9UmYpE64qhHFmhr1ew';//wX9NwuHnZU2ToO7GmGR9uw
 let MBTAApiKey = 'wX9NwuHnZU2ToO7GmGR9uw';
 let WUApiKey = '682f91fd7c03e86f';
 
 let busRoute;
+let strLat;
+let strLon;
 
 let MBTARoutesQuery = {
   api_key: MBTAApiKey,
   format: 'json'
 };
 
-let MBTABusStop1000Query = {
+let MBTABusStopQuery = {
   api_key: MBTAApiKey,
-  route: '69',
-  format: 'jsonp'
+  route: busRoute,
+  format: 'json'
 };
 
+// let WUConditionsQuery = {
+//
+//   format: 'json',
+// };
+
 let getDataFromApi = (searchTerm, query, callback) => {
-  console.log(searchTerm);
-  console.log(query);
-  console.log(callback);
+  // console.log(searchTerm);
+  // console.log(query);
+  // console.log(callback);
   $.getJSON(searchTerm, query, callback);
 }
-//
-// function getDataFromApi(searchTerm, query, callback) {
-//
-// }
+
+let getWUDataFromApi = (searchTerm, lat, lon, callback) => {
+  $.ajax({
+      url: `${searchTerm}${lat},${lon}.json`,
+      method: 'GET'
+    })
+    .done(function(data) {
+      console.log(data);
+    });
+};
 
 let displayRoutesData = data => {
 
@@ -61,20 +77,31 @@ let displayBusStopData = data => {
   //console.log('test');
 
   let resultElement;
-  //console.log(data.direction[0].stop);
+  //add the stop id for the other mbta query
   data.direction[0].stop.forEach(item => {
-    resultElement += ``;
+    resultElement += `<li data-lat='${item.stop_lat}' data-lon='${item.stop_lon}'>${item.stop_name}</li>`;
   });
-  $('.bus-stop-list').append(resultElement);
+  $('.bus-stop-list').html(resultElement);
 };
 
+let displayWUData = data => {
+  console.log(data);
+};
+
+$('.bus-stop-list').on('click', 'li', (event) => {
+  console.log(event.currentTarget);
+  strLat = event.currentTarget.getAttribute('data-lat');
+  strLon = event.currentTarget.getAttribute('data-lon');
+  getWUDataFromApi(WUEndPoint, strLat, strLon, displayWUData);
+});
 
 
 
 $('.bus-list').on('change', (event) => {
-  busRoute = event.currentTarget.value;
-  console.log(busRoute);
-  getDataFromApi(MBTABusStopEndPoint, MBTABusStop1000Query, displayBusStopData);
+  //busRoute = event.currentTarget.value;
+  MBTABusStopQuery.route = event.currentTarget.value;
+  //console.log(busRoute);
+  getDataFromApi(MBTABusStopEndPoint, MBTABusStopQuery, displayBusStopData);
 })
 
 function watchSubmit() {
