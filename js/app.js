82e63ddfac1ff2b24dbd70fef0f9e095abdbd035
vcordero07 <vcordero07@gmail.com>
2017-07-06 21:47:53 -0400

    wip-refactoring display data

--- js/app.js
+++ js/app.js
@@ -116,6 +116,163 @@ let generateRoutesData = (data) => {
   $('.selectpicker').selectpicker({});
 };
 
+let generateBusStopData = (data) => {
+  resultElement = '';
+  data.direction[busDirection].stop.forEach(item => {
+    resultElement += `
+    <li class='list-group-item'
+    data-lat='${item.stop_lat}'
+    data-lon='${item.stop_lon}'
+    data-stopid='${item.stop_id}'
+    >${item.stop_name}</li>
+    `;
+  });
+  $('.bus-stop-list').html(resultElement);
+};
+
+let generatePreditionsByStopData = (data) => {
+  resultElement = '';
+  //if there is not mode available for this route then display this message
+  if (!data.hasOwnProperty('mode')) {
+
+    resultElement = 'No predictions available for this bus stop at this time 1.';
+    $('.bus-valid-time').html(resultElement);
+    return;
+  }
+  //if there is data display pass
+  for (let i = 0; i < data.mode[0].route.length; i++) {
+    if (data.mode[0].route[i].route_id === busRouteID) {
+
+      let currentTime = new Date();
+      resultElement = `<h6>Valid as of ${getCurrentTime()}</h6>`;
+      //console.log("data.mode.route", data.mode[0].route[i]);
+      recursiveIteration(data.mode[0].route[i])
+
+      $('.bus-valid-time').html(resultElement);
+      break;
+    } else {
+      resultElement = 'No predictions available for this bus stop at this time 2.'
+      $('.bus-valid-time').html(resultElement);
+    }
+  }
+};
+
+let generateWeatherUndergroundData = (data) => {
+  resultElement = "";
+
+  resultElement = `<p
+  data-forecasturl='${data.current_observation.forecast_url}'
+  data-icon='${data.current_observation.icon}'
+  data-iconurl='${data.current_observation.icon_url}'
+  data-tempf='${data.current_observation.temp_f}'
+  data-weather='${data.current_observation.weather}'>
+  Current Weather: ${data.current_observation.weather} <br/>
+  Current Temp:${data.current_observation.temp_f} <br/>
+  </p>`;
+};
+
+let generateDarkSkyData = (data) => {
+  resultElement = "";
+
+  resultElement = `
+
+  <div class='weather-window'
+  data-icon='${data.currently.icon}>'
+  data-summary='${data.currently.summary}'
+  data-time='${data.currently.time}'
+  data-temperature='${data.currently.temperature}'>
+  <figure class="icons">
+    <canvas id="${data.currently.icon}" width="32" height="32">
+    </canvas>
+  </figure>
+  ${data.currently.temperature}&#176; in ${geoCity}<br/>
+  <h6> ${data.currently.summary}</h6>
+  </div>
+  `;
+
+  // if (data.currently.temperature >= 100) {
+  //   //console.log('#C0392D', data.currently.temperature);
+  //   $('body').css("background-color", "#C0392D");
+  // } else if (data.currently.temperature >= 90 && data.currently.temperature < 100) {
+  //   //console.log('#D35400', data.currently.temperature);
+  //   $('body').css("background-color", "#D35400");
+  // } else if (data.currently.temperature >= 80 && data.currently.temperature < 90) {
+  //   //console.log('#E67E22', data.currently.temperature);
+  //   $('body').css("background-color", "#E67E22");
+  // } else if (data.currently.temperature >= 70 && data.currently.temperature < 80) {
+  //   //console.log('#F39C12', data.currently.temperature);
+  //   $('body').css("background-color", "#F39C12");
+  // } else if (data.currently.temperature >= 60 && data.currently.temperature < 70) {
+  //   //console.log('#F1C40F', data.currently.temperature);
+  //   $('body').css("background-color", "#F1C40F");
+  // } else if (data.currently.temperature >= 50 && data.currently.temperature < 60) {
+  //   //console.log('#2ECC71', data.currently.temperature);
+  //   $('body').css("background-color", "#2ECC71");
+  // } else if (data.currently.temperature >= 40 && data.currently.temperature < 50) {
+  //   //console.log('#27AE60', data.currently.temperature);
+  //   $('body').css("background-color", "#27AE60");
+  // } else if (data.currently.temperature >= 30 && data.currently.temperature < 40) {
+  //   //console.log('#3498DB', data.currently.temperature);
+  //   $('body').css("background-color", "#3498DB");
+  // } else if (data.currently.temperature >= 20 && data.currently.temperature < 30) {
+  //   //console.log('#2980D9', data.currently.temperature);
+  //   $('body').css("background-color", "#2980D9");
+  // } else if (data.currently.temperature >= 10 && data.currently.temperature < 20) {
+  //   //console.log('#9B59B6', data.currently.temperature);
+  //   $('body').css("background-color", "#9B59B6");
+  // } else if (data.currently.temperature >= 0 && data.currently.temperature < 10) {
+  //   //console.log('#8E44AD', data.currently.temperature);
+  //   $('body').css("background-color", "#8E44AD");
+  // } else if (data.currently.temperature >= -10 && data.currently.temperature < 0) {
+  //   //console.log('#34495E', data.currently.temperature);
+  //   $('body').css("background-color", "#34495E");
+  // } else if (data.currently.temperature >= -20 && data.currently.temperature < -10) {
+  //   //console.log('#2C3E50', data.currently.temperature);
+  //   $('body').css("background-color", "#2C3E50");
+  // } else {
+  //   console.log('Warning:', "Please don't go outside!");
+  // };
+
+  $('.weather-message').html(resultElement);
+  getSkyIcons(data.currently.icon);
+};
+
+let generateGeocodingData = (data) => {
+  geoCity = '';
+
+  data.results[0].address_components.forEach(item => {
+    if (item.types.hasOwnProperty('0')) {
+      if (item.types[0] === 'locality') {
+        geoCity = item.long_name;
+        console.log('geoCity:', geoCity);
+        return geoCity;
+      }
+    }
+  });
+};
+
+let generateStopByLocationData = (data) => {
+  resultElement = '';
+  data.stop.forEach(item => {
+    resultElement += `
+    <li class='list-group-item'
+    data-lat='${item.stop_lat}'
+    data-lon='${item.stop_lon}'
+    data-stopid='${item.stop_id}'
+    data-distance='${item.distance}'
+    >${item.stop_name}</li>
+    `;
+  });
+  $('.bus-stop-list').html(resultElement);
+};
+
+let generateRoutesByStopData = (data) => {
+  data.mode[0].route.forEach(item => {
+    console.log('item.route_id:', item.route_id);
+    busRouteID = item.route_id;
+  });
+};
+
 let displayData = (data, display) => {
   let resultElement;
   switch (display) {
@@ -126,179 +283,38 @@ let displayData = (data, display) => {
       break;
 
     case 'BusStopData':
-
       console.log('displayBusStopData :', data);
-      resultElement = '';
-      data.direction[busDirection].stop.forEach(item => {
-        resultElement += `
-        <li class='list-group-item'
-        data-lat='${item.stop_lat}'
-        data-lon='${item.stop_lon}'
-        data-stopid='${item.stop_id}'
-        >${item.stop_name}</li>
-        `;
-      });
-      $('.bus-stop-list').html(resultElement);
+      generateBusStopData(data);
       break;
 
     case 'PreditionsByStopData':
-
       console.log('displayPreditionsByStop data: ', data);
-
-      resultElement = '';
-      //if there is not mode available for this route then display this message
-      if (!data.hasOwnProperty('mode')) {
-
-        resultElement = 'No predictions available for this bus stop at this time 1.';
-        $('.bus-valid-time').html(resultElement);
-        return;
-      }
-      //if there is data display pass
-      for (let i = 0; i < data.mode[0].route.length; i++) {
-        if (data.mode[0].route[i].route_id === busRouteID) {
-
-          let currentTime = new Date();
-
-          resultElement = `<h6>Valid as of ${getCurrentTime()}</h6>`;
-          //console.log("data.mode.route", data.mode[0].route[i]);
-          recursiveIteration(data.mode[0].route[i])
-
-          $('.bus-valid-time').html(resultElement);
-          break;
-        } else {
-          resultElement = 'No predictions available for this bus stop at this time 2.'
-          $('.bus-valid-time').html(resultElement);
-        }
-      }
+      generatePreditionsByStopData(data);
       break;
 
     case 'WUData':
-
       console.log("displayWUData: ", data);
-      resultElement = "";
-
-      resultElement = `<p
-      data-forecasturl='${data.current_observation.forecast_url}'
-      data-icon='${data.current_observation.icon}'
-      data-iconurl='${data.current_observation.icon_url}'
-      data-tempf='${data.current_observation.temp_f}'
-      data-weather='${data.current_observation.weather}'>
-      Current Weather: ${data.current_observation.weather} <br/>
-      Current Temp:${data.current_observation.temp_f} <br/>
-      </p>`;
-
+      generateWeatherUndergroundData(data);
       break;
 
     case 'DarkSkyData':
-
       console.log('DarkSkyData: ', data);
-      resultElement = "";
-
-      resultElement = `
-
-      <div class='weather-window'
-      data-icon='${data.currently.icon}>'
-      data-summary='${data.currently.summary}'
-      data-time='${data.currently.time}'
-      data-temperature='${data.currently.temperature}'>
-      <figure class="icons">
-        <canvas id="${data.currently.icon}" width="32" height="32">
-        </canvas>
-      </figure>
-      ${data.currently.temperature}&#176; in ${geoCity}<br/>
-      <h6> ${data.currently.summary}</h6>
-      </div>
-      `;
-
-      // if (data.currently.temperature >= 100) {
-      //   //console.log('#C0392D', data.currently.temperature);
-      //   $('body').css("background-color", "#C0392D");
-      // } else if (data.currently.temperature >= 90 && data.currently.temperature < 100) {
-      //   //console.log('#D35400', data.currently.temperature);
-      //   $('body').css("background-color", "#D35400");
-      // } else if (data.currently.temperature >= 80 && data.currently.temperature < 90) {
-      //   //console.log('#E67E22', data.currently.temperature);
-      //   $('body').css("background-color", "#E67E22");
-      // } else if (data.currently.temperature >= 70 && data.currently.temperature < 80) {
-      //   //console.log('#F39C12', data.currently.temperature);
-      //   $('body').css("background-color", "#F39C12");
-      // } else if (data.currently.temperature >= 60 && data.currently.temperature < 70) {
-      //   //console.log('#F1C40F', data.currently.temperature);
-      //   $('body').css("background-color", "#F1C40F");
-      // } else if (data.currently.temperature >= 50 && data.currently.temperature < 60) {
-      //   //console.log('#2ECC71', data.currently.temperature);
-      //   $('body').css("background-color", "#2ECC71");
-      // } else if (data.currently.temperature >= 40 && data.currently.temperature < 50) {
-      //   //console.log('#27AE60', data.currently.temperature);
-      //   $('body').css("background-color", "#27AE60");
-      // } else if (data.currently.temperature >= 30 && data.currently.temperature < 40) {
-      //   //console.log('#3498DB', data.currently.temperature);
-      //   $('body').css("background-color", "#3498DB");
-      // } else if (data.currently.temperature >= 20 && data.currently.temperature < 30) {
-      //   //console.log('#2980D9', data.currently.temperature);
-      //   $('body').css("background-color", "#2980D9");
-      // } else if (data.currently.temperature >= 10 && data.currently.temperature < 20) {
-      //   //console.log('#9B59B6', data.currently.temperature);
-      //   $('body').css("background-color", "#9B59B6");
-      // } else if (data.currently.temperature >= 0 && data.currently.temperature < 10) {
-      //   //console.log('#8E44AD', data.currently.temperature);
-      //   $('body').css("background-color", "#8E44AD");
-      // } else if (data.currently.temperature >= -10 && data.currently.temperature < 0) {
-      //   //console.log('#34495E', data.currently.temperature);
-      //   $('body').css("background-color", "#34495E");
-      // } else if (data.currently.temperature >= -20 && data.currently.temperature < -10) {
-      //   //console.log('#2C3E50', data.currently.temperature);
-      //   $('body').css("background-color", "#2C3E50");
-      // } else {
-      //   console.log('Warning:', "Please don't go outside!");
-      // };
-
-      $('.weather-message').html(resultElement);
-      getSkyIcons(data.currently.icon);
+      generateDarkSkyData(data);
       break;
 
     case 'GeocodingData':
       console.log('GeocodingData', data);
-      geoCity = '';
-
-      data.results[0].address_components.forEach(item => {
-        if (item.types.hasOwnProperty('0')) {
-          if (item.types[0] === 'locality') {
-            geoCity = item.long_name;
-            console.log('geoCity:', geoCity);
-            return geoCity;
-          }
-        }
-      });
-
+      generateGeocodingData(data);
       break;
 
     case 'StopByLocation':
       console.log('StopByLocationData:', data);
-      resultElement = '';
-      data.stop.forEach(item => {
-        resultElement += `
-        <li class='list-group-item'
-        data-lat='${item.stop_lat}'
-        data-lon='${item.stop_lon}'
-        data-stopid='${item.stop_id}'
-        data-distance='${item.distance}'
-        >${item.stop_name}</li>
-        `;
-      });
-      $('.bus-stop-list').html(resultElement);
-
+      generateStopByLocationData(data);
       break;
 
     case 'RoutesByStop':
-
-      //fix this
       console.log('RoutesByStopData:', data);
-      data.mode[0].route.forEach(item => {
-        console.log('item.route_id:', item.route_id);
-        busRouteID = item.route_id;
-      });
-
+      generateRoutesByStopData(data);
       break;
   }
 };
@@ -314,7 +330,7 @@ let recursiveIteration = (object) => {
         //found a property which is not an object, check for your conditions here
         if (property === 'pre_away') {
           minTime = (minTime === 0) ? object[property] : Math.min(object[property], minTime);
-          console.log('minTime:', minTime);
+          //console.log('minTime:', minTime);
           //
           resultElement = `<h3>${Math.round(minTime / 60)}</h3><h6>min<h6>`;
           $('.next-bus-predictions').html(resultElement);
